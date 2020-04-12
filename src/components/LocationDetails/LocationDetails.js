20f82b1fbe7d5b904adcc17b22fb5c4eeea2c62b
Tom Morris <thomasmorris823@gmail.com>
2020-04-12 15:40:25 +0100

    Make Dialogue a component and button styling

--- src/components/LocationDetails/LocationDetails.js
+++ src/components/LocationDetails/LocationDetails.js
@@ -6,6 +6,7 @@ import LocationDetailItems from "./LocationDetailItems/LocationDetailItems";
 import Button from "../UI/Button/Button";
 import Modal from "../UI/Modal/Modal";
 import Aux from "../../hoc/Aux/Aux";
+import Dialogue from "../UI/Dialogue/Dialogue";
 
 class LocationDetails extends Component {
   state = {
@@ -34,62 +35,40 @@ class LocationDetails extends Component {
         <div className={classes.DetailsButton}>
           {this.props.editing ? (
             <Aux>
-              <Button btnType={"Save"} clicked={this.props.initialSave}>
+              <Button btnType={"Success"} clicked={this.props.initialSave}>
                 Save
               </Button>
-              <Button btnType={"Edit"} clicked={this.props.savingCancelled}>
+              <Button btnType={"Danger"} clicked={this.props.savingCancelled}>
                 Cancel
               </Button>
             </Aux>
           ) : (
-            <Button btnType={"Edit"} clicked={this.props.edit}>
+            <Button btnType={"Danger"} clicked={this.props.edit}>
               Edit
             </Button>
           )}
         </div>
 
-        <Modal
-          show={this.props.saving}
-          modalClosed={this.props.savingCancelled}
-        >
-          <header>
-            <h2>Do you want to save?</h2>
-          </header>
-          <article className="content">
-            <p>Performing this action will update the database permanently if you chose to save.</p>
-          </article>
-          <footer>
-            <a
-              className={[classes.Button, classes.Success].join(" ")}
-              onClick={() => this.props.save(this.state.locationDetails)}
-            >
-              Save
-            </a>
-            <a
-              className={[classes.Button, classes.Danger].join(" ")}
-              onClick={this.props.savingCancelled}
-            >
-              Cancel
-            </a>
-          </footer>
-          {/* <Button
-            btnType={"Save"}
-            clicked={() => this.props.save(this.state.locationDetails)}
-          >
-            Yes
-          </Button>
-          <Button btnType={"Edit"} clicked={this.props.savingCancelled}>
-            Cancel
-          </Button> */}
-        </Modal>
-
         <Line classOverride="MainBody" />
-
         <LocationDetailItems
           locationDetails={this.props.locationDetails}
           editing={this.props.editing}
           updateField={this.handleFieldUpdate}
         />
+
+        <Modal
+          show={this.props.saving}
+          modalClosed={this.props.savingCancelled}
+        >
+          <Dialogue
+            title="Do you want to save?"
+            confirm={() => this.props.save(this.state.locationDetails)}
+            cancel={this.props.savingCancelled}
+          >
+            Performing this action will update the database permanently if you
+            chose to save.
+          </Dialogue>
+        </Modal>
       </div>
     );
   }
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,12 +3,12 @@ import React from "react";
 import classes from "./Button.css";
 
 const button = props => (
-  <button
+  <a
     className={[classes.Button, classes[props.btnType]].join(" ")}
     onClick={props.clicked}
   >
     {props.children}
-  </button>
+  </a>
 );
 
 export default button;
